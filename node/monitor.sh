66791fc558a4b5801b9638122a07accad027cc2b
vnskln <vnsklndev@gmail.com>
2022-11-01 03:22:19 +0100

    monitor.sh changed

--- node/monitor.sh
+++ node/monitor.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-for i in {1..11}; do { printf '%(%H:%M:%S)T'; echo -n " "; docker stats loadGenerator --no-stream --format "{{.CPUPerc}}"; } >> logs; done & for i in {1..20}; do { mpstat -P 0 1 1 | awk '{print $1, $3"%"}' | head -n4 | tail -n1; } >> logs2; done &
+loadgenpid=$(docker inspect -f '{{.State.Pid}}' loadGenerator)
+echo $loadgenpid
+for i in {1..300}; do { printf '%(%H:%M:%S)T'; echo -n " "; docker stats loadGenerator --no-stream --format "{{.CPUPerc}}"; } >> logs_docker; done & 
+for i in {1..300}; do { mpstat 2 1 | awk '{print $1, $3""}' | head -n4 | tail -n1; } >> logs_mpstat; done &
+for i in {1..300}; do { mpstat -P 0 2 1 | awk '{print $1, $3""}' | head -n4 | tail -n1; } >> logs_mpstat_0; done &
+for i in {1..300}; do { mpstat -P 1 2 1 | awk '{print $1, $3""}' | head -n4 | tail -n1; } >> logs_mpstat_1; done &
+for i in {1..300}; do { pidstat -p $loadgenpid 2 1 | awk '{print $1, $4""}' | head -n4 | tail -n1; } >> logs_pidstat; done &
+wait
+sed -i 's/\./,/g' logs_docker logs_mpstat logs_mpstat_0 logs_mpstat_1 logs_pidstat
+sed -i 's/%//g' logs_docker
+echo 'Monitoring done' 
